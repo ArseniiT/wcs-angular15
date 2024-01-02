ed3e2e3ee8d982e9c49d163009b4030c35d59725
arseniit <arseniit@gmail.com>
2024-01-02 02:06:22 +0100

    Add user sign in form based on FormBuilder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'wcs-angular15-formgroup';
+export class AppComponent implements OnInit{
+  userForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+      this.userForm = this.formBuilder.group({
+        firstName: ['', [Validators.required,Validators.minLength(2), Validators.maxLength(20)]],
+        secondName: ['', [Validators.required,Validators.minLength(2), Validators.maxLength(20)]],
+        credentials: this.formBuilder.group({
+          email: ['', [Validators.required,Validators.email]],
+          password: ['', [Validators.required,Validators.minLength(3), Validators.maxLength(8)]],
+        })
+      });
+  }
+
+  submit(){
+    console.log(this.userForm.value);
+  }
+
 }
